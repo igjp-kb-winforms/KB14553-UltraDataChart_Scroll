a19e40126b818164686498605624a4a27a38357d
mkamiishi <mkamiishi@infragistics.com>
2024-10-25 18:09:32 +0900

    初回コミット
    Initial commit

---
+++ UltraDataChart_Scroll/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace UltraDataChart_Scroll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ultraDataChart1 = new Infragistics.Win.DataVisualization.UltraDataChart();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraDataChart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ultraDataChart1
+            // 
+            this.ultraDataChart1.BackColor = System.Drawing.Color.White;
+            this.ultraDataChart1.CrosshairPoint = new Infragistics.Win.DataVisualization.Point(double.NaN, double.NaN);
+            this.ultraDataChart1.HorizontalViewScrollbarCornerRadius = double.NaN;
+            this.ultraDataChart1.HorizontalViewScrollbarHeight = double.NaN;
+            this.ultraDataChart1.HorizontalViewScrollbarInset = double.NaN;
+            this.ultraDataChart1.HorizontalViewScrollbarMaxOpacity = double.NaN;
+            this.ultraDataChart1.HorizontalViewScrollbarShouldAddAutoTrackInsets = true;
+            this.ultraDataChart1.HorizontalViewScrollbarStrokeThickness = double.NaN;
+            this.ultraDataChart1.HorizontalViewScrollbarTrackEndInset = double.NaN;
+            this.ultraDataChart1.HorizontalViewScrollbarTrackStartInset = double.NaN;
+            this.ultraDataChart1.Location = new System.Drawing.Point(12, 12);
+            this.ultraDataChart1.MarkerBrushes.Add(new Infragistics.Win.DataVisualization.SolidColorBrush(System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))))));
+            this.ultraDataChart1.Name = "ultraDataChart1";
+            this.ultraDataChart1.ScrollbarsAnimationDuration = 300;
+            this.ultraDataChart1.Size = new System.Drawing.Size(1037, 426);
+            this.ultraDataChart1.TabIndex = 0;
+            this.ultraDataChart1.Text = "ultraDataChart1";
+            this.ultraDataChart1.VerticalViewScrollbarCornerRadius = double.NaN;
+            this.ultraDataChart1.VerticalViewScrollbarInset = double.NaN;
+            this.ultraDataChart1.VerticalViewScrollbarMaxOpacity = double.NaN;
+            this.ultraDataChart1.VerticalViewScrollbarShouldAddAutoTrackInsets = true;
+            this.ultraDataChart1.VerticalViewScrollbarStrokeThickness = double.NaN;
+            this.ultraDataChart1.VerticalViewScrollbarTrackEndInset = double.NaN;
+            this.ultraDataChart1.VerticalViewScrollbarTrackStartInset = double.NaN;
+            this.ultraDataChart1.VerticalViewScrollbarWidth = double.NaN;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 446);
+            this.Controls.Add(this.ultraDataChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraDataChart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.DataVisualization.UltraDataChart ultraDataChart1;
+    }
+}
+
---
+++ UltraDataChart_Scroll/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Infragistics.Win.DataVisualization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltraDataChart_Scroll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var data = new EnergyDataSource();
+
+            var xAxis = new CategoryXAxis
+            {
+                Label = "Region",
+                DataSource = data
+            };
+
+            var yAxis1 = new NumericYAxis();
+            yAxis1.MinimumValue = 0;
+            yAxis1.MaximumValue = 10;
+
+            ColumnSeries series1 = new ColumnSeries();
+            series1.XAxis = xAxis;
+            series1.YAxis = yAxis1;
+            series1.ValueMemberPath = "Value1";
+            series1.DataSource = data;
+
+            ultraDataChart1.Axes.Add(xAxis);
+            ultraDataChart1.Axes.Add(yAxis1);
+            ultraDataChart1.Series.Add(series1);
+
+            ultraDataChart1.HorizontalViewScrollbarMode = SeriesViewerScrollbarMode.Persistent;
+            ultraDataChart1.WindowRect = new Infragistics.Win.DataVisualization.Rectangle(0, 0, .2, 1);
+        }
+    }
+
+    public class EnergyProduction
+    {
+        public string Region { get; set; }
+        public double Value1 { get; set; }
+    }
+
+    public class EnergyDataSource : List<EnergyProduction>
+    {
+        public EnergyDataSource()
+        {
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 50; i++)
+            {
+                this.Add(new EnergyProduction { Region = "Region" + i, Value1 = rnd.Next(1, 10) });
+            }
+        }
+    }
+}
